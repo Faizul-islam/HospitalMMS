480482e10a254af18b89ca5148f9d2688e85049b
Faizul Islam <faizula9090@gmail.com>
2019-12-04 00:28:53 +0600

    change

---
+++ HospitalMMS/Modules/PersonModels/Models/Persons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalMMS.Modules.PersonModels.Models
+{
+    public class Persons
+    {
+        public Int64 Id { get; set; }
+       
+        public String Name { get; set; }
+        public String PhoneNo { get; set; }
+        public String Email { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public GenderEnum? Gender { get; set; }
+        public String PresentAddress { get; set; }
+        public String PermanentAddress { get; set; }
+        public String PhotoPath { get; set; }
+        public int? IUser { get; set; }
+        public DateTime? IDate { get; set; }
+        public int? EUsaer { get; set; }
+        public DateTime? EDate { get; set; }
+
+    }
+}
